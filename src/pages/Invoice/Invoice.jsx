3e4e72a7da52eaf2e2e592d783621aee5e637f84
Jade <56938630+jadekang0611@users.noreply.github.com>
2021-11-16 17:26:25 -0800

    Added the basic form with state handlers

--- src/pages/Invoice/Invoice.jsx
+++ src/pages/Invoice/Invoice.jsx
@@ -1,10 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Invoice.scss';
 
 const Invoice = () => {
+  const [values, setValues] = useState({
+    description: '',
+    quantity: '',
+    cost: '',
+    listItems: [],
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
   return (
     <div className="invoice-container">
-      <h1>Invoice Page</h1>
+      <h1>Invoizer</h1>
+      <div className="add-form-container">
+        <form>
+          <div className="label-input-group">
+            <div>
+              <label htmlFor="trxDescription">Description</label>
+              <input
+                type="text"
+                placeholder="Enter transaction item"
+                name="description"
+                label="Description"
+                onChange={handleChange}
+                value={values.description}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="trxQuantity">Quantity</label>
+              <input
+                type="number"
+                placeholder="Enter quantity"
+                name="quantity"
+                label="Quantity"
+                onChange={handleChange}
+                value={values.quantity}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="trxCost">Cost</label>
+              <input
+                type="number"
+                placeholder="Enter unit cost"
+                name="cost"
+                label="Cost"
+                onChange={handleChange}
+                value={values.cost}
+                required
+              />
+            </div>
+            <input type="submit" value="Add item" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
