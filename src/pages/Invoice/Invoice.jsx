66533103ff6f1996391306c43521d4ad9615a26e
Jade <56938630+jadekang0611@users.noreply.github.com>
2021-11-16 17:18:11 -0800

    Modified the file structure

---
+++ src/pages/Invoice/Invoice.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Invoice.scss';
+
+const Invoice = () => {
+  return (
+    <div className="invoice-container">
+      <h1>Invoice Page</h1>
+    </div>
+  );
+};
+
+export default Invoice;
